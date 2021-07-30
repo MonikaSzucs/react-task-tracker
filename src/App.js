a7673a8c2200cc202e6be6fe99186f737bcd39d6
Monika <monika.silvia.s@hotmail.ca>
2021-07-30 16:19:53 -0700

    Finished creating button and Online Function

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className='container'>
       <Header title={"hello"}/>
-      <h1>Hello From React</h1>
+      {/* <h1>Hello From React</h1>
       <h2>Hello {name}</h2>
-      <h2>Hello {x ? "Yes" : "No"}</h2>
+      <h2>Hello {x ? "Yes" : "No"}</h2> */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 // import React from 'react'
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
+            {/* <Button color='blue' text='Hello 2'/>
+            <Button color='red' text='Hello 3'/> */}
         </header>
     )
 }
@@ -30,5 +37,11 @@ Header.defaultProps ={
 Header.propTypes = {
     title:PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//     color:'red',
+//     backgroundColor:'black'
+// }
 
 export default Header
