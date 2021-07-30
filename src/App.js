bddca646a353c4c6e95015f42f6edb29e8d0b2d9
Monika <monika.silvia.s@hotmail.ca>
2021-07-30 16:37:08 -0700

    Finished creating tasks with array
    Array was put in App.js so then the variables will be global if I put it just inside one of the components it is hard to move the variables around.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import Header from './components/Header'
-function App() {
-  const name = 'Brad'
-  const x = true
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
+const App = () => {
+  // const name = 'Brad'
+  // const x = true
+  const [tasks, setTasks] = useState(
+      [
+          {
+              id: 1,
+              text: 'Doctors Appointment',
+              day: 'Feb 5th at 2:30pm',
+              reminder: true,
+          },
+          {
+              id: 2,
+              text: 'Meeting at School',
+              day: 'Feb 6th at 1:30pm',
+              reminder: true
+          },
+          {
+              id: 3,
+              text: 'Food Shopping',
+              day: 'February 5th at 2:30pm',
+              reminder: false,
+          }
+      ]
+  )
   return (
     <div className='container'>
-      <Header title={"hello"}/>
+      <Header/>
+      <Tasks tasks={tasks}/>
       {/* <h1>Hello From React</h1>
       <h2>Hello {name}</h2>
       <h2>Hello {x ? "Yes" : "No"}</h2> */}
